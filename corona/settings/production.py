c5383187dd2322a67a2387931157b6f5e06de13e
Tim Claes <tim.claes@me.com>
2020-09-04 11:28:33 +0200

    updating allowed hosts

--- corona/settings/production.py
+++ corona/settings/production.py
@@ -12,6 +12,10 @@ WSGI_APPLICATION = 'uniekgroen.wsgi.application'
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
+SECURE_SSL_REDIRECT = True
+
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.aws.amazon.com', '.herokuapp.com', 'planetvirus.org', 'planetvirus.net', 'planetvirus.info']
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1', '.aws.amazon.com', '.herokuapp.com', 'www.planetvirus.org', 'www.planetvirus.net', 'www.planetvirus.info', '.planetvirus.org', '.planetvirus.net', '.planetvirus.info'
+]
